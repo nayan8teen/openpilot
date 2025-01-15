92161634beab303a4e76c2f7f223c350a030aabb
Kumar <36933347+rav4kumar@users.noreply.github.com>
2025-01-14 17:23:18 -0700

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -24,15 +24,15 @@ from openpilot.sunnypilot.selfdrive.controls.lib.dynamic_experimental_controller
 LON_MPC_STEP = 0.2  # first step is 0.2s
 A_CRUISE_MIN = -1.2
 
-A_CRUISE_MAX_VALS =   [2.0, 2.0,  2.0,  1.99,  1.25,  .71, .46,  .333,  .115]
-A_CRUISE_MAX_BP =     [0.,  1.,   6.,   8.,   11.,  20.,  25.,  30.,  55.]
-A_CRUISE_MIN_VALS =    [-0.24, -0.24,  -0.25, -1.0, -1.2]
-A_CRUISE_MIN_BP =      [0.,    5.,     11.,   20.,  30.]
+#A_CRUISE_MAX_VALS =   [2.0, 2.0,  2.0,  1.99,  1.25,  .71, .46,  .333,  .115]
+#A_CRUISE_MAX_BP =     [0.,  1.,   6.,   8.,   11.,  20.,  25.,  30.,  55.]
+#A_CRUISE_MIN_VALS =    [-0.24, -0.24,  -0.25, -1.0, -1.2]
+#A_CRUISE_MIN_BP =      [0.,    5.,     11.,   20.,  30.]
 
-#A_CRUISE_MAX_VALS = [3.0, 1.7, 1.3, 0.7, 0.6, 0.44, 0.32, 0.22, 0.16, 0.0078]
-#A_CRUISE_MAX_BP =   [0.,  3,   6.,  8.,  11., 15.,  20.,  25.,  30.,  55.]
-#A_CRUISE_MIN_VALS = [-0.65,  -0.60,  -0.70, -0.70,  -0.65, -0.65]
-#A_CRUISE_MIN_BP =   [0.,     0.07,   10.,   20.,    30.,   55.]
+A_CRUISE_MAX_VALS = [3.0, 1.7, 1.3, 0.7, 0.6, 0.44, 0.32, 0.22, 0.16, 0.0078]
+A_CRUISE_MAX_BP =   [0.,  3,   6.,  8.,  11., 15.,  20.,  25.,  30.,  55.]
+A_CRUISE_MIN_VALS = [-0.65,  -0.60,  -0.70, -0.70,  -0.65, -0.65]
+A_CRUISE_MIN_BP =   [0.,     0.07,   10.,   20.,    30.,   55.]
 
 CONTROL_N_T_IDX = ModelConstants.T_IDXS[:CONTROL_N]
 ALLOW_THROTTLE_THRESHOLD = 0.5
