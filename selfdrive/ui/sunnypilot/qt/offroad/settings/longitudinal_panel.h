f40d8ec32b3ea7008ba9fcf8a65035f6de923c2b
discountchubbs <alexgrant990@gmail.com>
2025-08-03 11:57:10 -0400

    Dynamic Experimental Controller Configurations

--- selfdrive/ui/sunnypilot/qt/offroad/settings/longitudinal_panel.h
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/longitudinal_panel.h
@@ -9,8 +9,10 @@
 
 #include "selfdrive/ui/sunnypilot/qt/offroad/settings/longitudinal/custom_acc_increment.h"
 #include "selfdrive/ui/sunnypilot/qt/offroad/settings/longitudinal/speed_limit/speed_limit_settings.h"
+#include "selfdrive/ui/sunnypilot/qt/offroad/settings/longitudinal/dec_controller.h"
 #include "selfdrive/ui/sunnypilot/qt/offroad/settings/settings.h"
 #include "selfdrive/ui/sunnypilot/qt/widgets/scrollview.h"
+#include "selfdrive/ui/sunnypilot/ui.h"
 
 class LongitudinalPanel : public QWidget {
   Q_OBJECT
@@ -38,4 +40,7 @@ private:
   ParamControl *dynamicExperimentalControl = nullptr;
   SpeedLimitSettings *speedLimitScreen;
   PushButtonSP *speedLimitSettings;
+
+  DecControllerSubpanel *decScreen;
+  ButtonControlSP *decManageBtn = nullptr;
 };
