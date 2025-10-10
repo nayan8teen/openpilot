a37ca548cba2aa2eb625efb6db48843b1761934e
nayan <nayan8teen@gmail.com>
2025-10-10 10:53:20 -0400

    Hyundai Radar Toggle

--- common/params_keys.h
+++ common/params_keys.h
@@ -209,6 +209,7 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"SubaruStopAndGo", {PERSISTENT | BACKUP, BOOL, "0"}},
     {"SubaruStopAndGoManualParkingBrake", {PERSISTENT | BACKUP, BOOL, "0"}},
     {"TeslaCoopSteering", {PERSISTENT | BACKUP, BOOL, "0"}},
+    {"HyundaiRadar", {PERSISTENT | BACKUP, INT, "0"}},
 
     // Dynamic Experimental Controller
     {"DynamicExperimentalControl", {PERSISTENT | BACKUP, BOOL, "0"}},
--- sunnypilot/selfdrive/car/interfaces.py
+++ sunnypilot/selfdrive/car/interfaces.py
@@ -111,7 +111,8 @@ def initialize_params(params) -> list[dict[str, Any]]:
 
   # hyundai
   keys.extend([
-    "HyundaiLongitudinalTuning"
+    "HyundaiLongitudinalTuning",
+    "HyundaiRadar"
   ])
 
   # subaru
